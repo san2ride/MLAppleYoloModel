6153d4e739bd845d6633c4d4f63fc7b969bdd9c8
Jason <sanchezjason@me.com>
2024-06-03 12:46:06 -0600

    ML_yolo/image_recognition/ML_model_yolo- ImageRecognitionView

--- YoloImageRecognition/YoloImageRecognition/ImageRecognitionView.swift
+++ YoloImageRecognition/YoloImageRecognition/ImageRecognitionView.swift
@@ -6,10 +6,54 @@
 //
 
 import SwiftUI
+import CoreML
+import PhotosUI
+
+@State private var probs: [String: Double] = [: ]
+@State private var uiImage: UIImage? = UIImage(named: "stop-sign")
+@State private var selectedPhotoItem: PhotosPickerItem? = nil
+@State private var isCameraSelected: Bool = false
 
 struct ImageRecognitionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(uiImage: uiImage!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300)
+            HStack {
+                PhotosPicker(selection: $selectedPhotoItem, matching: .images, photoLibrary: .shared()) {
+                   Text("Select a Photo")
+                }
+                
+                Button("Camera") {
+                    isCameraSelected = true
+                }.buttonStyle(.bordered)
+            }
+            
+            Button("Predict") {
+                
+            }.buttonStyle(.borderedProminent)
+            
+           
+        }
+        .onChange(of: selectedPhotoItem, perform: { selectedPhotoItem in
+            selectedPhotoItem?.loadTransferable(type: Data.self, completionHandler: { result in
+                print(result)
+                switch result {
+                    case .success(let data):
+                        if let data {
+                            uiImage = UIImage(data: data)
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                }
+            })
+        })
+        .sheet(isPresented: $isCameraSelected, content: {
+            ImagePicker(image: $uiImage, sourceType: .camera)
+        })
+        .padding()
     }
 }
 
