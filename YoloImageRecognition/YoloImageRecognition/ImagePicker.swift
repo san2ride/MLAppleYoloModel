32f0a65534b7c584c2802200eb5e6cfac9c3291d
Jason <sanchezjason@me.com>
2024-06-03 12:53:35 -0600

    ML_yolo/image_recognition/ML_model_yolo- ImagePicker

---
+++ YoloImageRecognition/YoloImageRecognition/ImagePicker.swift
@@ -0,0 +1,58 @@
+//
+//  ImagePicker.swift
+//  YoloImageRecognition
+//
+//  Created by Jason Sanchez on 6/3/24.
+//
+
+import UIKit
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Environment(\.dismiss) private var dismiss
+    typealias UIViewControllerType = UIImagePickerController
+    typealias Coordinator = ImagePickerCoordinator
+    @Binding var image: UIImage?
+    var sourceType: UIImagePickerController.SourceType = .camera
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePickerCoordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+    
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        
+        var picker: ImagePicker
+        
+        init(_ picker: ImagePicker) {
+            self.picker = picker
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            
+            if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                self.picker.image = uiImage
+            }
+            
+            self.picker.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            self.picker.dismiss()
+        }
+    }
+}
