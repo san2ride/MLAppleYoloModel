16fb401720cdc2cd46e63ca5977252a76445291d
Jason <sanchezjason@me.com>
2024-06-03 12:40:15 -0600

    ML_yolo/image_recognition/ML_model_yolo- setup

---
+++ YoloImageRecognition/YoloImageRecognition/ImageRecognitionView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageRecognitionView.swift
+//  YoloImageRecognition
+//
+//  Created by Jason Sanchez on 6/3/24.
+//
+
+import SwiftUI
+
+struct ImageRecognitionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageRecognitionView()
+}
---
+++ YoloImageRecognition/YoloImageRecognition/YoloImageRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoloImageRecognitionApp.swift
+//  YoloImageRecognition
+//
+//  Created by Jason Sanchez on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct YoloImageRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ImageRecognitionView()
+        }
+    }
+}
